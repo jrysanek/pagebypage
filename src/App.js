c7734896633f02e7e25cca503c1fb1b84a862390
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-04-27 18:17:03 -0400

    axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, {useEffect, UseState } from 'react';
 
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const []
+
+  const getBooks = async (e) => {
+    e.preventDefault
+   const response = await axios({
+     baseUrl: 'http://openlibrary.org/books/OL1M.json',
+     method: 'get',
+     params: {
+       name: input, //name whatever i store input
+       author:
+     }
+    });
+  };
   //call api 
   //render books and title on page, wish list option on page
   //when user clicks link, opens up more information.
@@ -12,8 +26,10 @@ function App() {
           Page by Page
       </header>
       <p>Home</p>
+      <form onSubmit={getBooks}>
       <input type="text" placeholder=" title or author name"></input>
       <button type="button">Submit</button>
+      </form>
       <div className="search-results">
         <main className="books">
 
