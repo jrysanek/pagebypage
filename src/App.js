5aff089207e19d89b5035f8a8acf30ffc8b6307d
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-04-28 11:34:30 -0400

    working on handling user input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import convert from 'xml-js';
 import axios from 'axios';
 
 function App() {
   // const [title, userTitle] = ('')
-  const [author, userAuthor] = useState([]) 
+  const [input, userInput] = useState([]);
+
+
 
 
   useEffect(() => {
@@ -12,41 +15,52 @@ function App() {
     getBooks()
   }, [])
   const getBooks = async (e) => {
-    if (e) e.preventDefault();
-   const response = await axios({
-     baseURL: 'http://openlibrary.org/books/OL1M.json',
-     method: 'get',
-     params: {             
-       author: 'Anne McCaffrey'
-       
-     }
-    });
-    // userAuthor()
+    // e.preventDefault();
+    let apiKey = process.env.REACT_APP_API_KEY
+    let userInput = userInput
+    //assign a variable for user input here 
+    const response = await axios(`https://corsanywhere.herokuapp.com/https://www.goodreads.com/search/index.xml?key=${apiKey}&q=${userInput}`)
+
+    const resJSON = convert.xml2json(response.data, { compact: true, spaces: 4 })
+    console.log(JSON.parse(resJSON))
+
+
     console.log(response)
   };
- 
 
 
 
 
-  
+
+
+
+
+
+
+
   //call api 
   //render books and title on page, wish list option on page
   //when user clicks link, opens up more information.
   return (
     <div className="App">
       <header>
-          Page by Page
+        Page by Page
       </header>
       <p>Home</p>
-      <form onSubmit={getBooks}>
-      <input type="text" placeholder="author name" onChange={(e) => userAuthor(e.target.value)}></input>
-      <button>Submit</button>
+      <form >
+
+        <input type="text"
+          placeholder="author name or title"
+          value={input}
+          onChange={(e) => userInput(e.target.value)}>
+        </input>
+
+        <button onClick={() => getBooks()}>Submit</button>
       </form>
       <div className="search-results">
         <main className="books">
 
-        {/* <Results /> */}
+          {/* <Results /> */}
         </main>
       </div>
     </div>
