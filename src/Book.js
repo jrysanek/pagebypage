c1a167a039b994bad96c9489be69124be96e018f
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-05-01 01:47:45 -0400

    styled iphone results and home

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ export default function Book(props) {
     const book= props.book;
     const index= props.index;
     return (
-        <div className="results">
+        <div className="results" >
         <img className="imgRes" src={book.best_book.image_url._text} />
         <button className="wishlist" onClick={() => props.addToWishlist(book)}>Wishlist</button>
         <Link to={`/Showbook/${index}`}><h2>{book.best_book.title._text}</h2></Link>
